27c10083df9076962638f019c16bf1db870f5ca9
oemmanuel <owusu2020emmanuel@gmail.com>
2023-11-29 10:25:19 -0800

    modify the structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {Routes,Route} from 'react-router-dom';
-import Main from './frontend/pages/landing/main';
-import Home from './frontend/pages/landing/home';
-import Ehr from './frontend/pages/dashboard/ehr';
-import Login from './frontend/pages/landing/login';
-import Erropage from './frontend/pages/landing/error';
-import Dashboard from './frontend/pages/dashboard/dashboard';
-import Over from './frontend/pages/dashboard/over';
+import Main from './pages/landing/main';
+import Home from './pages/landing/home';
+import Ehr from './pages/dashboard/ehr';
+import Login from './pages/landing/login';
+import Erropage from './pages/landing/error';
+import Dashboard from './pages/dashboard/dashboard';
+import Over from './pages/dashboard/over';
 
 function App() {
   
